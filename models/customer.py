b89bf7317c1ca68b73af83a1111e214ff268041b
Kristen Steele <ksteelephoto@gmail.com>
2021-01-20 14:44:18 -0600

    adds get_customer_by_email, get_animals_by_location, get_employees_by_location, get_animals_by_status

--- models/customer.py
+++ models/customer.py
@@ -1,7 +1,9 @@
 class Customer():
-    def __init__(self, id, email, password, name):
+
+    def __init__(self, id, name, address, email = "", password = ""):
         self.id = id
+        self.name = name
+        self.address = address
         self.email = email
         self.password = password
-        self.name = name
     
